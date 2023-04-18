1478867e7e1a5f5c62f7c2c721d4efe9bb02b486
Kevengg <kevinandre.engell@gmail.com>
2023-04-18 12:06:33 +0200

    added search bar droppdown to header

--- src/includes/header.jsx
+++ src/includes/header.jsx
@@ -1,16 +1,22 @@
+import { Link } from "react-router-dom";
 import frLogo from "../img/forbrukerradet_logo.svg";
 import frLogoSmall from "../img/forbrukerradet_logo_symbol.svg";
+import { useState } from "react";
 
 console.log("main js loaded");
+
 var menuOpen = false;
 function openMenu() {
+    if (document.getElementById("headerSearchHide").classList != "headerSearchHidden") {
+        document.getElementById("headerSearchHide").classList.toggle("headerSearchHidden");
+    }
     if (menuOpen == false) {
         document.getElementById("bar1").classList = "burgerBar bar1X";
         document.getElementById("bar2").classList = "burgerBar bar2X";
         document.getElementById("bar3").classList = "burgerBar bar3X";
         document.getElementById("menuBack").style = "background-color: #04aac4;";
         menuOpen = true;
-        document.getElementById("menuHide").classList = "shown";
+        document.getElementById("menuHide").classList = "";
         document.body.style = "position: fixed;";
     } else if (menuOpen == true) {
         document.getElementById("bar1").classList = "burgerBar";
@@ -18,8 +24,20 @@ function openMenu() {
         document.getElementById("bar3").classList = "burgerBar";
         document.getElementById("menuBack").style = "background-color: none";
         menuOpen = false;
-        document.getElementById("menuHide").classList = "hidden";
+        document.getElementById("menuHide").classList = "menuHidden";
         document.body.style = "position: static;";
+    }
+}
+
+function openSearch() {
+    if (menuOpen == true) {
+        openMenu();
+    }
+    document.getElementById("headerSearchHide").classList.toggle("headerSearchHidden");
+    if (document.body.style.position == "fixed") {
+        document.body.style = "position: static;";
+    } else {
+        document.body.style = "position: fixed;";
     }
 }
 
@@ -31,6 +49,8 @@ window.addEventListener("keypress", function (e) {
 });
 
 export default function Headers() {
+    const [headerSearch, setHeaderSearch] = useState("");
+
     return (
         <header>
             <a href="#" id="hopp" style={{ position: "absolute" }}>
@@ -54,7 +74,13 @@ export default function Headers() {
                     <a href="/kontakt_oss"> kontakt oss </a>
                 </div>
 
-                <div className="headerSearchHolder" role="search" aria-label="Søk" tabIndex="0">
+                <div
+                    className="headerSearchHolder"
+                    role="search"
+                    aria-label="Søk"
+                    tabIndex="0"
+                    onClick={() => openSearch()}
+                >
                     <i
                         style={{ color: "#9a9a9a" }}
                         className="fa-solid fa-magnifying-glass fa-sm"
@@ -87,7 +113,36 @@ export default function Headers() {
                     </div>
                 </div>
             </nav>
-            <div className="hidden" id="menuHide">
+            <div className="headerSearchHidden" id="headerSearchHide">
+                <div id="headerSearchBox">
+                    <div id="headerSearchBarSpacer">
+                        <div id="headerSearchBar">
+                            <input
+                                type="search"
+                                name=""
+                                id="headerSearch"
+                                placeholder="Søk"
+                                value={headerSearch}
+                                onChange={(e) => {
+                                    setHeaderSearch(e.target.value);
+                                }}
+                            />
+                            <a
+                                href={
+                                    (headerSearch.length >= 3 || true) &&
+                                    `/search?search=${headerSearch}`
+                                }
+                                id="headerSearchBtn"
+                            >
+                                <div>
+                                    <i className="fa-solid fa-magnifying-glass fa-lg"></i> Søk
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="menuHidden" id="menuHide">
                 <div className="menuWidth">
                     <nav id="menu">
                         <div id="viHjelperDeg" className="menuColum">
